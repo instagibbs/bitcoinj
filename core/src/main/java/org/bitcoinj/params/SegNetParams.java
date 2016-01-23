26725a490e66c802233d8f84a4c8a947964134aa
Gregory Sanders <gsanders87@gmail.com>
2016-01-23 11:30:45 -0500

    Added segnet params

--- core/src/main/java/org/bitcoinj/params/SegNetParams.java
+++ core/src/main/java/org/bitcoinj/params/SegNetParams.java
@@ -42,20 +42,25 @@ public class SegNetParams extends AbstractBitcoinNetParams {
         port = 28332;
         addressHeader = 111;
         p2shHeader = 196;
+        p2wpkh = 3;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader, p2wpkh };
         dumpedPrivateKeyHeader = 239;
-        genesisBlock.setTime(1296688602L);
-        genesisBlock.setDifficultyTarget(0x1d00ffffL);
-        genesisBlock.setNonce(414098458);
+        genesisBlock.setTime(1296688602L); //TODO
+        genesisBlock.setDifficultyTarget(0x1d00ffffL); //TODO
+        genesisBlock.setNonce(414098458); //TODO
         spendableCoinbaseDepth = 100;
         subsidyDecreaseBlockCount = 210000;
-        String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
+        String genesisHash = "0d5b9c518ddf053fcac71730830df4526a9949c08f34acf6a1d30464d22f02aa";//genesisBlock.getHashAsString();
+        //checkState(genesisHash.equals("000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943")); //TODO
         alertSigningKey = Utils.HEX.decode("04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a");
 
         dnsSeeds = new String[] {
-                "testnet-seed.bitcoin.schildbach.de", // Andreas Schildbach
-                "testnet-seed.bitcoin.petertodd.org"  // Peter Todd
+                //"testnet-seed.bitcoin.schildbach.de", // Andreas Schildbach
+                //"testnet-seed.bitcoin.petertodd.org"  // Peter Todd
+                "104.243.38.34",
+                "104.155.1.158",
+                "119.246.245.241",
+                "46.101.235.82"
         };
         addrSeeds = null;
         bip32HeaderPub = 0x043587CF;
